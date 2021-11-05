0a41783ed138ac1f0f542bb15246ed3f76ce04f9
Shelka4444 <87785129+Shelka4444@users.noreply.github.com>
2021-11-05 09:50:41 -0700

    Add files via upload

--- charts.js
+++ charts.js
@@ -88,6 +88,33 @@ function buildCharts(sample) {
       title: "Top 10 Bacteria Cultures Found",
     };
     // 10. Use Plotly to plot the data with the layout. 
-    Plotly.newPlot("bar", barData, barLayout)
-  });
+    Plotly.newPlot("bar", barData, barLayout);
+
+  // 1. Create the trace for the bubble chart.
+      var bubbleData = [{
+        x: ids,
+        y: values,
+        text: labels,
+        mode: "markers",
+        marker: {
+          size: values,
+          color: values,
+          colorscale: "Viridis"
+        }
+      }];
+  
+  // 2. Create the layout for the bubble chart.
+      var bubbleLayout = {
+        title: "Bacteria Cultures Per Sample",
+        xaxis: {title: "OTU ID"},
+        automargin: true,
+        hovermode: "closest"
+        
+    };
+  
+  // 3. Use Plotly to plot the data with the layout.
+    Plotly.newPlot("bubble", bubbleData, bubbleLayout); 
+});
+
 }
+
